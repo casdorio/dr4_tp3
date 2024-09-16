d9762d8ac464a23cd98ef3c148d892364e04fbe0
casdorio <casdorio@gmail.com>
2024-09-15 21:59:42 -0300

    start

---
+++ src/main/java/com/carlos/infnet/dr4_tp3/Dr4Tp3Application.java
@@ -0,0 +1,13 @@
+package com.carlos.infnet.dr4_tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr4Tp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr4Tp3Application.class, args);
+	}
+
+}
